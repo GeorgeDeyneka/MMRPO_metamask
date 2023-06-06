2ed23482e163671d9a8b51d33b4ea1831be5ca50
GeorgeDeyneka <zhoradeynecka@gmail.com>
2023-06-06 11:36:07 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0",
+        },
+        {
+          charset: "UTF-8",
+        },
+      ],
+      title: "MMPRO Landing",
+    },
+  },
+  css: ["@/assets/scss/style.scss"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="container home">
+    <h1 class="home__title">Metamask Login</h1>
+  </main>
+</template>
+<script setup lang="ts"></script>
+
+<style lang="scss">
+// .home {
+//   &__title {
+//     color: #000;
+//   }
+// }
+</style>
